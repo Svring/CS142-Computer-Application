3d76b44ef14addd3b07be5eee66220e9369b1204
Svring <lxf83553899@163.com>
2022-10-07 08:38:23 +0800

    project4 header setup

---
+++ project4/components/header/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Header;
